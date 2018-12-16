970ad97c28756c629752cdf4e9a91f3d1b1e3e07
Guy Theuws <guy.theuws@luminis.eu>
2018-12-16 16:13:41 +0100

    Do not use star imports

---
+++ ecj/src/main/java/ec/app/testar/UnitTest.java
@@ -0,0 +1,24 @@
+package ec.app.testar;
+
+import org.junit.Test;
+
+import java.util.TreeMap;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+
+    @Test
+    public void testReader() {
+        ResultsReader reader = new ResultsReader();
+        TreeMap<String, String> result;
+
+        result = reader.getResults(25);
+
+        for (String key : result.keySet()) {
+            System.out.println(key + ": " + result.get(key));
+        }
+        assertNotNull(result);
+    }
+
+}
