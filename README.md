db8516f4f4589ae235fbb9096ebf00e3ed6aa7ca
Guy Theuws <guy_theuws@hotmail.com>
2019-09-04 20:40:38 +0200

    Add files

--- README.md
+++ README.md
@@ -4,15 +4,7 @@ This repository contains results and source code of various research experiments
 
 ## The branch "prepare-ecj-for-testar" contains the source code of ECJ extended with classes required for TESTAR (https://github.com/GuyT07/thesis/tree/feature/prepare-ecj-for-testar)
 
-## Results
+## The ECJ folder contains the genetic programming results
 
-### Preparation
-
-#### Notepad - GP (MG)
-
-10 actions - 5 sequences per strategy - Population size 1000 --> stopped run prematurely (had to work on VM)
-https://github.com/GuyT07/thesis/tree/preparation/notepad-3200-runs
-
-50 actions - 1 sequence per strategy - Population size 100
-https://github.com/GuyT07/thesis/tree/preparation/notepad-400-runs
+## The TESTAR folder contains TEST* results
  
--- git-add.bat
+++ git-add.bat
@@ -1,15 +1,15 @@
 git branch "%1"
 git checkout "%1"
-git add resource/**
+git add resources/**
 git add ecj-archive/**
 git add output/**
 git add ecj-simplified/**
 
-xcopy %USERPROFILE%\workspace\thesis\ecj\evolution.properties %USERPROFILE%\Desktop\testar\evolution.properties /Y
-xcopy %USERPROFILE%\workspace\thesis\ecj\src\main\resources\ec\app\testar\%2 %USERPROFILE%\Desktop\testar\%2 /Y
-xcopy %USERPROFILE%\workspace\thesis\ecj\out.stat %USERPROFILE%\Desktop\testar\out.stat /Y
-xcopy %USERPROFILE%\workspace\thesis\ecj\meta.stat %USERPROFILE%\Desktop\testar\meta.stat /Y
-xcopy %USERPROFILE%\workspace\thesis\ecj\front.stat %USERPROFILE%\Desktop\testar\front.stat /Y
+xcopy %USERPROFILE%\IdeaProjects\thesis\ecj\evolution.properties %USERPROFILE%\Desktop\testar\evolution.properties /Y
+xcopy %USERPROFILE%\IdeaProjects\thesis\ecj\src\main\resources\ec\app\testar\%2 %USERPROFILE%\Desktop\testar\%2 /Y
+xcopy %USERPROFILE%\IdeaProjects\thesis\ecj\out.stat %USERPROFILE%\Desktop\testar\out.stat /Y
+xcopy %USERPROFILE%\IdeaProjects\thesis\ecj\meta.stat %USERPROFILE%\Desktop\testar\meta.stat /Y
+xcopy %USERPROFILE%\IdeaProjects\thesis\ecj\front.stat %USERPROFILE%\Desktop\testar\front.stat /Y
 
 git add evolution.properties
 git add %2
